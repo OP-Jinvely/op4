464f88d5e77a393d28384183ff1bd92ad4603599
Hyowon Kim <87758380+HyowonKim1116@users.noreply.github.com>
2021-08-15 13:25:44 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -534,7 +534,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
 
   # Camera is not outputting frames at a constant framerate
   EventName.cameraMalfunction: {
-    ET.PERMANENT: NormalPermanentAlert("Camera Malfunction", "Contact Support"),
+    ET.PERMANENT: NormalPermanentAlert("", ""),
   },
 
   # Unused
