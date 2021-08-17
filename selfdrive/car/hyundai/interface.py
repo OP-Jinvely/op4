262544a1acade5bf03f70defc8459f998ad8e463
stonerains <72440538+stonerains@users.noreply.github.com>
2021-08-17 09:27:35 +0900

    Update interface.py
    튜닝

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -251,9 +251,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.innerLoopGainBP = [0.]
       ret.lateralTuning.indi.innerLoopGainV = [3.5] # third tune. Highest value that still gives smooth control. Effects turning into curves.
       ret.lateralTuning.indi.outerLoopGainBP = [0.]
-      ret.lateralTuning.indi.outerLoopGainV = [2.0] # forth tune. Highest value that still gives smooth control. Effects lane centering.
+      ret.lateralTuning.indi.outerLoopGainV = [2.1] # forth tune. Highest value that still gives smooth control. Effects lane centering.
       ret.lateralTuning.indi.timeConstantBP = [0.]
-      ret.lateralTuning.indi.timeConstantV = [1.8] # second tune. Lowest value with smooth actuation. Avoid the noise of actuator gears thrashing.
+      ret.lateralTuning.indi.timeConstantV = [1.6] # second tune. Lowest value with smooth actuation. Avoid the noise of actuator gears thrashing.
       ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
       ret.lateralTuning.indi.actuatorEffectivenessV = [1.95] # first tune. Lowest value without oversteering. May vary with speed.
 
